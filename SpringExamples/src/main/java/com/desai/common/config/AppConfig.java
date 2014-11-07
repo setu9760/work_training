d48b8f17fe493ff4ee1298b8f11681efd7443247
desai <desai@BRIWS0609.sst.stp>
2014-11-07 14:36:25 +0000

    aa

--- SpringExamples/src/main/java/com/desai/common/config/AppConfig.java
+++ SpringExamples/src/main/java/com/desai/common/config/AppConfig.java
@@ -2,10 +2,11 @@ package com.desai.common.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import com.desai.common.config.CsvGeneratorConfig;
 
 @Configuration
-@Import({ CsvGeneratorConfig.class, JsonGeneratorConfig.class })
+// @Import({ CsvGeneratorConfig.class, JsonGeneratorConfig.class })
 // @Import({ NumberConfig.class })
 public class AppConfig {
-
+	CsvGeneratorConfig config;
 }
