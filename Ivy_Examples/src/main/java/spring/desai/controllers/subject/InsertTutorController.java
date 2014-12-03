a12ebbec9f5d11c2d17e152fac74748b929e6271
desai <desai@BRIWS0609.sst.stp>
2014-12-03 16:34:11 +0000

    annotated the controllers

--- Ivy_Examples/src/main/java/spring/desai/controllers/subject/InsertTutorController.java
+++ Ivy_Examples/src/main/java/spring/desai/controllers/subject/InsertTutorController.java
@@ -1,5 +1,10 @@
 package spring.desai.controllers.subject;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(value="tutor/insert")
 public class InsertTutorController {
 
 }
--- Ivy_Examples/src/main/java/spring/desai/controllers/tutor/SearchSubjectController.java
+++ Ivy_Examples/src/main/java/spring/desai/controllers/tutor/SearchSubjectController.java
@@ -1,5 +1,10 @@
 package spring.desai.controllers.tutor;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(value="subject/search")
 public class SearchSubjectController {
 
 }
